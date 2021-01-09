20f9091c84eb546716a113be4818c8839026841a
Emirhan Engin <me@emirhanengin.com>
2021-01-09 18:17:50 +0300

    Deleted useless row class

--- components/blogs.js
+++ components/blogs.js
@@ -6,33 +6,31 @@ function Blogs({ title, items }) {
 	return (
 		<section className="blogs">
 			<div className="container">
-				<div className="row">
-					<Title text={title} />
-					
-					{items ? (
-						<div className="row">
-							{items.map((item, i) => (
-								<div className="col-lg-4" key={i}>
-									<div className="blog-item">
-										<Link href={`/blogs/${item.slug}`}>
-											<a>
-												<div className="blog-item-wallpaper">
-													<img src={item.image} alt="" />
-												</div>
-												<div className="blog-item-title">{item.title}</div>
-												<div className="blog-item-description">{item.introduction}</div>
-											</a>
-										</Link>
-									</div>
+				<Title text={title} />
+				
+				{items ? (
+					<div className="row">
+						{items.map((item, i) => (
+							<div className="col-lg-4" key={i}>
+								<div className="blog-item">
+									<Link href={`/blogs/${item.slug}`}>
+										<a>
+											<div className="blog-item-wallpaper">
+												<img src={item.image} alt="" />
+											</div>
+											<div className="blog-item-title">{item.title}</div>
+											<div className="blog-item-description">{item.introduction}</div>
+										</a>
+									</Link>
 								</div>
-							))}
-						</div>
-					) : (
-						<div className="blogs-error">
-							<p>There is no blog posts yet.</p>
-						</div>
-					)}
-				</div>
+							</div>
+						))}
+					</div>
+				) : (
+					<div className="blogs-error">
+						<p>There is no blog posts yet.</p>
+					</div>
+				)}
 			</div>
 		</section>
 	);
